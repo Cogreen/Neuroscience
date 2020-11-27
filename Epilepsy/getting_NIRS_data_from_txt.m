0355abd8ddb73ec98cf1acb78d9d8e9da4b0cb8b
Hyunjung Kim <35440430+Cogreen@users.noreply.github.com>
2020-11-27 17:59:41 +0900

    Update getting_NIRS_data_from_txt.m

--- Epilepsy/getting_NIRS_data_from_txt.m
+++ Epilepsy/getting_NIRS_data_from_txt.m
@@ -1,7 +1,9 @@
-# Title: Getting NIRS data from *.txt files 
+#############################################################################
+# Getting NIRS data from *.txt files 
 # Description: When NIRS data is exported as *.txt file, you use this code!
 # Date: 2019.12
 # Writer: Cogreen
+#############################################################################
 
 cd('Y:\CLINIC\01APM\01NIRS_EEG\Data\20191226');
 
